64b2af2a18db5d0da55573a3592863f380c9f601
36129382023.1N <danisegd@hotmail.com>
2024-10-03 19:57:33 -0300

    Cria atributos do model de carrinho

--- src/main/java/br/senac/rj/grupo1/carrinhodecompra/entities/Carrinho.java
+++ src/main/java/br/senac/rj/grupo1/carrinhodecompra/entities/Carrinho.java
@@ -1,11 +1,24 @@
 package br.senac.rj.grupo1.carrinhodecompra.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Entity
+@Table(name = "cart")
 public class Carrinho {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(columnDefinition = "VARCHAR", nullable = false)
+    private int status;
+
+    @Column(name = "total", columnDefinition = "DECIMAL")
+    private BigDecimal valorTotal;
 
     private List<ItemCarrinho> itens;
 
