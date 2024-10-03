8150affa36d31ccc419e71e3f26f56f95b841a6d
36129382023.1N <danisegd@hotmail.com>
2024-10-03 19:36:06 -0300

    first commit

---
+++ src/main/java/br/senac/rj/grupo1/carrinhodecompra/Application.java
@@ -0,0 +1,13 @@
+package br.senac.rj.grupo1.carrinhodecompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/rj/grupo1/carrinhodecompra/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.rj.grupo1.carrinhodecompra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
