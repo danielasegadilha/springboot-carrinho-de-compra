c1d1badc0204cb22fba1eebadd40b6eb76216be7
36129382023.1N <danisegd@hotmail.com>
2024-10-03 20:02:31 -0300

    Finaliza getter and setters do carrinho

--- src/main/java/br/senac/rj/grupo1/carrinhodecompra/entities/Carrinho.java
+++ src/main/java/br/senac/rj/grupo1/carrinhodecompra/entities/Carrinho.java
@@ -3,6 +3,7 @@ package br.senac.rj.grupo1.carrinhodecompra.entities;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -14,12 +15,19 @@ public class Carrinho {
     @Column(name = "id", nullable = false)
     private int id;
 
+    @Column(name = "dt_criacao")
+    private LocalDateTime dataCriacao;
+
     @Column(columnDefinition = "VARCHAR", nullable = false)
     private int status;
 
     @Column(name = "total", columnDefinition = "DECIMAL")
     private BigDecimal valorTotal;
+    @Column(name = "usuario_id")
+    private long usuarioId;
 
+    @ManyToMany
+    @JoinColumn(name="carrinhoId")
     private List<ItemCarrinho> itens;
 
 //    public double calcularTotal() {
