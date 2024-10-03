0a44652ec7404cf707272cb498f9f1fc11c7ced2
ADS 2023.1N <36129382023.1N@senacrjedu.local>
2024-10-03 20:50:44 -0300

    Criacao do service de carrinho

---
+++ src/main/java/br/senac/rj/grupo1/carrinhodecompra/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package br.senac.rj.grupo1.carrinhodecompra.exception;
+
+public class EntidadeNaoEncontradaException extends RuntimeException{
+    public EntidadeNaoEncontradaException(String message) {
+        super(message);
+    }
+}
--- src/main/java/br/senac/rj/grupo1/carrinhodecompra/services/CarrinhoService.java
+++ src/main/java/br/senac/rj/grupo1/carrinhodecompra/services/CarrinhoService.java
@@ -1,4 +1,31 @@
 package br.senac.rj.grupo1.carrinhodecompra.services;
 
+import org.springframework.stereotype.Service;
+
+import com.senac.victor.gerenciamentosalas2.exception.EntidadeNaoEncontradaException;
+
+import br.senac.rj.grupo1.carrinhodecompra.entities.Carrinho;
+import br.senac.rj.grupo1.carrinhodecompra.repository.CarrinhoRepository;
+import jakarta.transaction.Transactional;
+
+@Service
 public class CarrinhoService {
+	
+	private final CarrinhoRepository carrinhoRepository;
+	
+	public CarrinhoService(CarrinhoRepository carrinhoRepository) {
+		this.carrinhoRepository = carrinhoRepository;
+	}
+	
+	@Transactional
+	public Carrinho createCarrinho(Carrinho carrinho) {
+		return carrinhoRepository.save(carrinho)
+	}
+	
+	@Transactional Carrinho updateCarrinho(Carrinho carrinho) {
+		if (!carrinhoRepository.existsById(carrinho.getId())) {
+			throw new EntidadeNaoEncontradaException("Carrinho com ID " + id + " não encontrado para deleção");
+		}
+		return carrinhoRepository.save(carrinho);
+	}
 }
