989e231d1e45359d27ebadb849936bfad137815c
Tales Costa <talescostam@gmail.com>
2024-10-03 21:25:06 -0300

    refatorando coluna status da tabela carts para ser int

--- src/main/java/br/senac/rj/grupo1/carrinhodecompra/repository/CarrinhoRepository.java
+++ src/main/java/br/senac/rj/grupo1/carrinhodecompra/repository/CarrinhoRepository.java
@@ -14,10 +14,10 @@ import br.senac.rj.grupo1.carrinhodecompra.entities.Carrinho;
 @Repository
 public interface CarrinhoRepository extends JpaRepository<Carrinho, Integer>{
 	
-	Optional<Carrinho> findByIdAndStatusNot(int id, String status);
+	Optional<Carrinho> findByIdAndStatusNot(int id, int status);
 
-	@Query("SELECT c FROM Carrinho c WHERE c.status = :status")
-	List<Carrinho> findCarrinhosAtivos(@Param("status") String status);
+	@Query("SELECT c FROM Carrinho c WHERE c.status != -1")
+	List<Carrinho> findCarrinhosAtivos();
 	
 	@Modifying
 	@Query(value = "UPDATE carts c SET c.status = -1 WHERE c.id = :id", nativeQuery = true)
